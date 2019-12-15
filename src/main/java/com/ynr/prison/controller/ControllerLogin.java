ab68397fc6d3b6638f1cecf1536fdd20cce7f0fa
yassine315 <yassim.lmahi@gmail.com>
2019-12-15 19:29:41 +0100

    l'interface Home

--- src/main/java/com/ynr/prison/controller/ControllerLogin.java
+++ src/main/java/com/ynr/prison/controller/ControllerLogin.java
@@ -1,14 +1,28 @@
 package com.ynr.prison.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ControllerLogin implements Initializable {
+	
+
+	private Scene scene;
+	private Stage primaryStage;
+	
+	private AnchorPane newAnchorPane;
+	
+	@FXML
+	private AnchorPane anchorPane;
 	
 	@FXML
 	private Button buttonLogin;
@@ -24,13 +38,30 @@ public class ControllerLogin implements Initializable {
 	
 	@FXML
 	private void sauthentifier() {
-		userName.setText("yeeeees");
+		
+		scene = anchorPane.getScene(); 
+		primaryStage = (Stage)scene.getWindow();
+		
+		FXMLLoader loader = new FXMLLoader(this.getClass().getClassLoader().getResource("Home.fxml"));
+		
+		try {
+			newAnchorPane = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		scene.setRoot(newAnchorPane);
+		primaryStage.setMaximized(true);
+		
 	}
+	
+
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.ynr.prison {
     requires javafx.fxml;
 	requires javafx.graphics;
     exports com.ynr.prison;
-    exports com.ynr.prison.controller;
     
     opens com.ynr.prison.controller to javafx.fxml;
 }
