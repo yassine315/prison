af3249214074b4325ddc1bc3951ab3295a8e2508
yassine315 <yassim.lmahi@gmail.com>
2019-12-19 22:40:07 +0100

    integration de l'interface prisonnier au app

--- src/main/java/com/ynr/prison/controller/ControllerHome.java
+++ src/main/java/com/ynr/prison/controller/ControllerHome.java
@@ -1,11 +1,15 @@
 package com.ynr.prison.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 
 public class ControllerHome implements Initializable {
 	
@@ -39,6 +43,29 @@ public class ControllerHome implements Initializable {
 	
 	@FXML
 	private void prisonnier() {
+		FXMLLoader loader = new FXMLLoader(this.getClass().getClassLoader().getResource("Prisonnier.fxml"));
+		
+		Region newContainer = new Region();
+		
+		try {
+			newContainer =(Region) loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		container.getChildren().clear();
+		container.getChildren().add(newContainer);
+		
+		newContainer.prefHeightProperty().bind(container.heightProperty());
+		newContainer.prefWidthProperty().bind(container.widthProperty());
+		
+	
+		
+	
+		
 		
 	}
 	@FXML
